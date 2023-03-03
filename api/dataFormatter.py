a68a73ba0acf095ef32d38b38f9c93da980bd8a4
TomLack <tjlack@virginmedia.com>
2023-03-03 12:25:21 +0000

    changes to dataFormatter

--- api/dataFormatter.py
+++ api/dataFormatter.py
@@ -64,44 +64,56 @@ class dataFormatter:
             "productNumber": productNumber
         }
     
-    def temperatureSensor(this,
-                          project_id:str,device_id:str,productNumber:int,
-                          network_signalStrength:int=0,network_rssi:int=0,network_updateTime:str="time",
-                          network_ccon_id:int=0,network_ccon_signalStrength:int=0,network_ccon_rssi:int=0,
-                          battery_percentage:int=0,battery_updateTime:str="time",
-                          temp_temperature:float=0,temp_updateTime:str="time"):
+
+    def sensor(this,
+            project_id:str,device_id:str,sensorType:str,productNumber:int,
+            network_signalStrength:int=0,network_rssi:int=0,network_updateTime:str="time",
+            network_ccon_id:int=0,network_ccon_signalStrength:int=0,network_ccon_rssi:int=0,
+            battery_percentage:int=0,battery_updateTime:str="time",
+            temp_temperature:float=0,temp_updateTime:str="time",
+            humidity_temperature:float=0,humidity_relative:float=0,humidity_updateTime:str="time",
+            co2_ppm:int=0,co2_updateTime:str="time"):
         """
-        A JSON formatter for temperature sensors
+        A JSON formatter for all sensor types
         """
+        match sensorType:
+            case "temperature":
+                event=this.temperatureEvent(temperature=temp_temperature,updateTime=temp_updateTime)
+            case "humidity":
+                event=this.humidityEvent(temperature=humidity_temperature,humidity=humidity_relative,updateTime=humidity_updateTime)
+            case "co2":
+                event=this.co2Event(ppm=co2_ppm,updateTime=co2_updateTime)
+            case _:
+                return {"error":"invalid sensor type: "+sensorType}
         return {
-                    "name": "/projects/"+project_id+"/devices/"+device_id,
-                    "type": "temperature",
-                    "labels": {},
-                    "reported": {
-                        "networkStatus": {
-                            "signalStrength": network_signalStrength,
-                            "rssi": network_rssi,
-                            "updateTime": network_updateTime,
-                            "cloudConnectors": [
-                                {
-                                "id": network_ccon_id,
-                                "signalStrength": network_ccon_signalStrength,
-                                "rssi": network_ccon_rssi
-                                }
-                            ],
-                            "transmissionMode": "LOW_POWER_STANDARD_MODE"
-                        },
-                        "batteryStatus": {
-                            "percentage": battery_percentage,
-                            "updateTime": battery_updateTime
-                        },
-                        "temperature": {
-                        "value": temp_temperature,
-                        "updateTime": temp_updateTime
-                        }
+                "name": "/projects/"+project_id+"/devices/"+device_id,
+                "type": sensorType,
+                "labels": {},
+                "reported": {
+                    "networkStatus": {
+                        "signalStrength": network_signalStrength,
+                        "rssi": network_rssi,
+                        "updateTime": network_updateTime,
+                        "cloudConnectors": [
+                            {
+                            "id": network_ccon_id,
+                            "signalStrength": network_ccon_signalStrength,
+                            "rssi": network_ccon_rssi
+                            }
+                        ],
+                        "transmissionMode": "LOW_POWER_STANDARD_MODE"
                     },
-                    "productNumber": productNumber
-                }
+                    "batteryStatus": {
+                        "percentage": battery_percentage,
+                        "updateTime": battery_updateTime
+                    },
+                    sensorType:event[sensorType]
+                },
+                "productNumber": productNumber
+            }
+
+        
+    
 
     def temperatureEvent(this,temperature:float,updateTime:int):
         """"
@@ -114,14 +126,152 @@ class dataFormatter:
             }
         }
 
-    def humiditySensor(this):
-        pass
+    
 
-    def humidityEvent(this):
-        pass
+    def humidityEvent(this,temperature:float=0,humidity:float=0,updateTime:str="time"):
+        """
+        A JSON formatter for humidity events
+        """
+        return {
+            "humidity": {
+                "temperature": temperature,
+                "relativeHumidity": humidity,
+                "updateTime": updateTime
+            }
+        }
 
-    def co2Sensor(this):
-        pass
+    
 
-    def co2Event(this):
-        pass
+    def co2Event(this,ppm:int=0,updateTime:str="time"):
+        """
+        A JSON formatter for co2 events
+        """
+        return {
+            "co2": {
+                "ppm": ppm,
+                "updateTime": updateTime
+            }
+        }
+
+
+# The following methods have been superseeded by the function sensor()
+# However these have not been deleted until further testing
+
+    # def temperatureSensor(this,
+    #                       project_id:str,device_id:str,productNumber:int,
+    #                       network_signalStrength:int=0,network_rssi:int=0,network_updateTime:str="time",
+    #                       network_ccon_id:int=0,network_ccon_signalStrength:int=0,network_ccon_rssi:int=0,
+    #                       battery_percentage:int=0,battery_updateTime:str="time",
+    #                       temp_temperature:float=0,temp_updateTime:str="time"):
+    #     """
+    #     A JSON formatter for temperature sensors
+    #     """
+    #     return {
+    #                 "name": "/projects/"+project_id+"/devices/"+device_id,
+    #                 "type": "temperature",
+    #                 "labels": {},
+    #                 "reported": {
+    #                     "networkStatus": {
+    #                         "signalStrength": network_signalStrength,
+    #                         "rssi": network_rssi,
+    #                         "updateTime": network_updateTime,
+    #                         "cloudConnectors": [
+    #                             {
+    #                             "id": network_ccon_id,
+    #                             "signalStrength": network_ccon_signalStrength,
+    #                             "rssi": network_ccon_rssi
+    #                             }
+    #                         ],
+    #                         "transmissionMode": "LOW_POWER_STANDARD_MODE"
+    #                     },
+    #                     "batteryStatus": {
+    #                         "percentage": battery_percentage,
+    #                         "updateTime": battery_updateTime
+    #                     },
+    #                     "temperature": {
+    #                     "value": temp_temperature,
+    #                     "updateTime": temp_updateTime
+    #                     }
+    #                 },
+    #                 "productNumber": productNumber
+    #             }
+
+    # def humiditySensor(this,
+    #                       project_id:str,device_id:str,productNumber:int,
+    #                       network_signalStrength:int=0,network_rssi:int=0,network_updateTime:str="time",
+    #                       network_ccon_id:int=0,network_ccon_signalStrength:int=0,network_ccon_rssi:int=0,
+    #                       battery_percentage:int=0,battery_updateTime:str="time",
+    #                       humidity_temperature:float=0,humidity_relative:float=0,humidity_updateTime:str="time"):
+    #     """
+    #     A JSON formatter for humidity sensors
+    #     """
+    #     return {
+    #                 "name": "/projects/"+project_id+"/devices/"+device_id,
+    #                 "type": "temperature",
+    #                 "labels": {},
+    #                 "reported": {
+    #                     "networkStatus": {
+    #                         "signalStrength": network_signalStrength,
+    #                         "rssi": network_rssi,
+    #                         "updateTime": network_updateTime,
+    #                         "cloudConnectors": [
+    #                             {
+    #                             "id": network_ccon_id,
+    #                             "signalStrength": network_ccon_signalStrength,
+    #                             "rssi": network_ccon_rssi
+    #                             }
+    #                         ],
+    #                         "transmissionMode": "LOW_POWER_STANDARD_MODE"
+    #                     },
+    #                     "batteryStatus": {
+    #                         "percentage": battery_percentage,
+    #                         "updateTime": battery_updateTime
+    #                     },
+    #                     "humidity": {
+    #                         "temperature": humidity_temperature,
+    #                         "relativeHumidity": humidity_relative,
+    #                         "updateTime": humidity_updateTime
+    #                     }
+    #                 },
+    #                 "productNumber": productNumber
+    #             }
+    
+    # def co2Sensor(this,
+    #                 project_id:str,device_id:str,productNumber:int,
+    #                 network_signalStrength:int=0,network_rssi:int=0,network_updateTime:str="time",
+    #                 network_ccon_id:int=0,network_ccon_signalStrength:int=0,network_ccon_rssi:int=0,
+    #                 battery_percentage:int=0,battery_updateTime:str="time",
+    #                 co2_ppm:int=0,co2_updateTime:str="time"):
+    #     """
+    #     A JSON formatter for co2 sensors
+    #     """
+    #     return {
+    #                 "name": "/projects/"+project_id+"/devices/"+device_id,
+    #                 "type": "temperature",
+    #                 "labels": {},
+    #                 "reported": {
+    #                     "networkStatus": {
+    #                         "signalStrength": network_signalStrength,
+    #                         "rssi": network_rssi,
+    #                         "updateTime": network_updateTime,
+    #                         "cloudConnectors": [
+    #                             {
+    #                             "id": network_ccon_id,
+    #                             "signalStrength": network_ccon_signalStrength,
+    #                             "rssi": network_ccon_rssi
+    #                             }
+    #                         ],
+    #                         "transmissionMode": "LOW_POWER_STANDARD_MODE"
+    #                     },
+    #                     "batteryStatus": {
+    #                         "percentage": battery_percentage,
+    #                         "updateTime": battery_updateTime
+    #                     },
+    #                     "co2": {
+    #                         "ppm": co2_ppm,
+    #                         "updateTime": co2_updateTime
+    #                     }
+    #                 },
+    #                 "productNumber": productNumber
+    #             }
+    
--- api/main.py
+++ api/main.py
@@ -3,41 +3,15 @@ from deviceEmulator import *
 from dataFormatter import *
 from enum import Enum
 
-title="Test API with FastAPI"
-description="""
+Title={
+    "app":"Test API with FastAPI"
+}
+Desc={
+"app":"""
 # For Team Project and Professionalism
 ## KV6002
-
-"""
-
-de=deviceEmulator()
-myFormat=dataFormatter()
-app = FastAPI(title,description=description)
-
-class Tags(Enum):
-    default = "default"
-    ds = "Disruptive Systems Endpoints"
-    devices="Devices & Labels"
-    labels="Devices & Labels"
-    none = "Not Implemented"
-
-@app.get("/")
-async def root():
-    return {"message": "Hello, World!"}
-
-@app.get("/projects")
-async def list_projects(query:str="",pageSize:int=10,pageToken:int=0):
-    returnList=de.getProjectList()[pageToken*pageSize:(pageToken+1)*pageSize]
-    return {"projects":returnList,"nextPageToken":"c0un66ecie6seakamrlg"}
-
-@app.get("/projects/{project}",tags=[Tags.none],description="NOT CURRENTLY IN USE---PAGE UNDER CONSTRUCTION")
-async def get_a_single_project(project):
-    return {
-        "requested project": project,
-        "message": "page under construction"
-    }
-
-deviceList_description="""
+""",
+"deviceList":"""
 shows generated list of devices, 5 ccons and 25 temp sensors, can be filtered by using the parameter deviceTypes
 
 params in use:
@@ -53,8 +27,55 @@ unused params:
 - productNumbers:list[str]|None=Query(default=None)
 - pageSize:int=None
 - pageToken:str=None
-"""
-@app.get("/projects/{project}/devices",tags=[Tags.devices],description=deviceList_description)
+""",
+"device":"Returns the given projects and device number in an otherwise empty temp sensor JSON object"
+}
+tags_metadata = [
+    {
+        "name": "default",
+        "description": "The defualt place for new endpoints."
+    },
+    {
+        "name": "Devices & Labels",
+        "description": "DS endpoints about devices and labels."
+    },
+    {
+        "name": "Organizations & Projects",
+        "description": "DS endpoints about organizations and projects."
+    },
+    {
+        "name": "Not Implemented",
+        "description": "These endpoints have not been implemented yet."
+    }
+]
+
+de=deviceEmulator()
+myFormat=dataFormatter()
+app = FastAPI(title=Title["app"],description=Desc["app"],openapi_tags=tags_metadata)
+
+
+
+@app.get("/",tags=["default"])
+async def root():
+    return {"message": "Hello, World!"}
+
+
+
+@app.get("/projects",tags=["Organizations & Projects"])
+async def list_projects(query:str="",pageSize:int=10,pageToken:int=0):
+    returnList=de.getProjectList()[pageToken*pageSize:(pageToken+1)*pageSize]
+    return {"projects":returnList,"nextPageToken":"c0un66ecie6seakamrlg"}
+
+@app.get("/projects/{project}",tags=["Not Implemented"],description="NOT CURRENTLY IN USE---PAGE UNDER CONSTRUCTION")
+async def get_a_single_project(project):
+    return {
+        "requested project": project,
+        "message": "page under construction"
+    }
+
+
+
+@app.get("/projects/{project}/devices",tags=["Devices & Labels"],description=Desc["deviceList"])
 async def list_sensors_and_cloud_devices(project:str,deciveIds:list[str]|None=Query(default=None),deviceTypes: list[str]|None=Query(default=None),labelFilters:list[str]|None=Query(default=None),orderBy:str=None,query:str=None,productNumbers:list[str]|None=Query(default=None),pageSize:int=None,pageToken:str=None):
         output=[]
         if deviceTypes==None:
@@ -66,8 +87,7 @@ async def list_sensors_and_cloud_devices(project:str,deciveIds:list[str]|None=Qu
                 output+=(de.getCconList())
         return {"devices":output}
 
-@app.get("/projects/{project}/devices/{device}",description="Returns the given projects and device number in an otherwise empty temp sensor JSON object")
-async def get_a_single_device(project:str,device:str):
-    productNumber=0
-    output = myFormat.temperatureSensor(project,device,productNumber) 
+@app.get("/projects/{project}/devices/{device}",tags=["Devices & Labels"],description=Desc["device"])
+async def get_a_single_device(project:str,device:str,sensorType:str):
+    output = myFormat.sensor(project_id=project,device_id=device,sensorType=sensorType,productNumber=0)
     return output
